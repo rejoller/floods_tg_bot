473df4836c10894fb7e80b331ae05db645e19044
rejoller <130197750+rejoller@users.noreply.github.com>
2024-11-18 15:09:55 +0700

    добавлены новости в ручном режиме, нужно разнести код по разным частям

--- handlers/dialog_subscribe.py
+++ handlers/dialog_subscribe.py
@@ -19,10 +19,11 @@ from sqlalchemy import and_, delete, select
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.models import FCategoriesSubscriptions, FCategories, DFloodKrudor, Municipalities, MunicSubscriptions
+from database.models import FCategoriesSubscriptions, FCategories, DFloodKrudor, Municipalities, MunicSubscriptions, DFloodAggoAndChs
 from database.engine import session_maker
 
-from utils.decline_phrase import decline_phrase
+from utils.decline_phrase import decline_phrase, choose_plural
+from icecream import ic
 
 
 class MySG(StatesGroup):
@@ -177,6 +178,7 @@ async def add_munsub(session_: Optional[AsyncSession], user_id, municipality_id)
     
     query = select(MunicSubscriptions.map_id).where(user_id == user_id)
     result = await session_.execute(query)
+    result = result.all()
 
     if not result:
         insert_query = (insert(MunicSubscriptions).values(user_id=user_id, map_id=map_id, date_subscribed = dt.now())).on_conflict_do_nothing()
@@ -365,6 +367,75 @@ async def check_news(callback: CallbackQuery, session: AsyncSession, dialog_mana
     await callback.message.answer(text=response)
         
 
+
+# sd_qfh_result - H  
+# ap_epp_result -    AS
+# ap_efz_result -    AX
+# ap_epp_wta_people - AT
+# ap_efz_wta_people - AY
+
+
+# 
+async def check_news_ago(callback: CallbackQuery, session: AsyncSession, dialog_manager: DialogManager):
+    async with session_maker() as session_:
+        query = select(DFloodAggoAndChs.municipality, DFloodAggoAndChs.date_incident, DFloodAggoAndChs.operating_mode,DFloodAggoAndChs.type_flood, DFloodAggoAndChs.settlement,
+                       DFloodAggoAndChs.omd_wlwb_cwl, DFloodAggoAndChs.omd_wlwb_cl, DFloodAggoAndChs.sd_qfh_result, DFloodAggoAndChs.sd_qft_result, DFloodAggoAndChs.sd_qft_estate_territory, DFloodAggoAndChs.ap_qvfz_result,
+                       DFloodAggoAndChs.ap_qvfz_children, DFloodAggoAndChs.ap_epp_result, DFloodAggoAndChs.ap_efz_result, DFloodAggoAndChs.ap_epp_wta_people, DFloodAggoAndChs.ap_efz_wta_people)
+        
+        response = await session_.execute(query)
+        result = response.all()
+        
+        df = pd.DataFrame(result)
+        df['date_incident'] = df['date_incident'].apply(lambda x: x.strftime('%d.%m.%Y') if pd.notnull(x) else x)
+        df['danger_icon'] = df.apply(lambda row: '🟢' if row['operating_mode'] == 'Повседневная деятельность' else '🔴', axis=1)
+        
+        response = ''
+        for i in df.iterrows():
+            
+            response += f"{i[1]['danger_icon']} {i[1]['municipality']}\n"
+            response += f"{i[1]['date_incident']}. {i[1]['type_flood']}.\n"
+            response += f"н/п {i[1]['settlement']}\n"
+            evacuated_people = i[1]['ap_epp_result'] + i[1]['ap_efz_result']
+            temp_holded = i[1]['ap_efz_wta_people'] + i[1]['ap_epp_wta_people']
+            difference = i[1]['omd_wlwb_cwl'] - i[1]['omd_wlwb_cl']
+            
+            if difference > 0:
+                response += f"Текущий уровень воды {i[1]['omd_wlwb_cwl']} см (критический {i[1]['omd_wlwb_cl']} см + {difference} см)\n"
+            if difference < 0:
+                response += f"Текущий уровень воды {i[1]['omd_wlwb_cwl']} см (критический {i[1]['omd_wlwb_cl']} см - {abs(difference)} см)\n"
+            if difference == 0:
+                response += f"Текущий уровень воды {i[1]['omd_wlwb_cwl']} см (критический {i[1]['omd_wlwb_cl']} см)\n"
+
+            home_form = await choose_plural(i[1]['sd_qfh_result'], ["дом", "дома", "домов"])
+            home_form_1 = await choose_plural(i[1]['sd_qfh_result'], ['Затоплен', 'Затоплены', 'Затоплено'])
+            
+            affected_form = await choose_plural(i[1]['ap_qvfz_result'], ['человек', 'человека', 'человек'])
+            teritory_form = await choose_plural(i[1]['sd_qfh_result'], ['территория', 'территории', 'территорий'])
+            evacuated_people_form = await choose_plural(evacuated_people, ['человек', 'человека', 'человек'])
+            temp_holded_form = await choose_plural(temp_holded, ['человек', 'человека', 'человек'])
+            
+            affected_form_1 = await choose_plural(i[1]['ap_qvfz_children'], ['Пострадал','Пострадали','Пострадало'])
+            evacuated_people_form_1 = await choose_plural(evacuated_people, ['Эвакуирован','Эвакуированы','Эвакуировано'])
+            temp_holded_1 = await choose_plural(temp_holded, ['Размещен','Размещены','Размещено'])
+            
+            
+            
+            response += f"{home_form_1} всего {i[1]['sd_qfh_result']}  {home_form}"
+            if i[1]['sd_qfh_result'] > 0:
+                response += f", всего {i[1]['sd_qfh_result']} {teritory_form}\n"
+            else :
+                response += "\n"
+                
+            response += f"{affected_form_1} {i[1]['ap_qvfz_result']} {affected_form}, из них {i[1]['ap_qvfz_children']} детей\n"
+            response += f"{evacuated_people_form_1} {evacuated_people} {evacuated_people_form}\n"
+            response += f"{temp_holded_1} в пунктах временного содержания {temp_holded} {temp_holded_form}\n\n"
+            
+            
+        await callback.message.answer(text=response)
+                
+                
+                
+
 multi2 = Multiselect(
     Format("✅ {item[0]}"),
     Format("☑️{item[0]}"),
@@ -394,6 +465,7 @@ dialog = Dialog(
         Button(Const("Выбрать муниципальные образования"), id="4", on_click=button1_clicked),
         Button(Const("🆘Помощь"), id="2", on_click=button1_clicked),
         Button(Const("🗞️Получить новости"), id="3", on_click=check_news),
+        Button(Const("Получить новости по подтоплениям ГО и ЧС"), id="5", on_click=check_news_ago),
         state=MySG.window1,
         getter=window1_get_data),
     
--- utils/decline_phrase.py
+++ utils/decline_phrase.py
@@ -25,3 +25,15 @@ async def decline_phrase(n):
             road_form = forms_road[2]
 
     return f"на {n} {section_form} {road_form}"
+
+
+
+async def choose_plural(num, forms):
+
+    if num % 10 == 1 and num % 100 != 11:
+        return forms[0]
+    elif 2 <= num % 10 <= 4 and (num % 100 < 10 or num % 100 >= 20):
+        return forms[1]
+    else:
+        return forms[2]
+
